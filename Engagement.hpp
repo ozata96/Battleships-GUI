1860fd1a2920caebe8e8cf510b2b8fae711b834b
Arafeh, Omar <omar.arafeh20@imperial.ac.uk>
2021-10-07 16:53:50 +0100

    Finalized targetting enemy ships on the second game board

--- Engagement.hpp
+++ Engagement.hpp
@@ -9,21 +9,23 @@
 
 using namespace std;
 
-class Engagement 
+class Engagement
 {
-    public:
-        Engagement();
-        ~Engagement();
+public:
+    Engagement();
+    ~Engagement();
+
+    int ConvertRow(int row);
+    int ConvertCol(int col);
 
     // public:
-        // Interaction
-        void MakeMove();
-        void Fight(vector<string> &enemyGrid);
-        void Strike(int row, int col, vector<string> &grid);
-        
-        // Consequences
-        void Miss(int row, int col, vector<string> &grid);
-        void Hit(int row, int col, vector<string> &grid);
+    // Interaction
+    void MakeMove();
+    void Strike(int row, int col, vector<string> &grid, vector<string> &display);
+
+    // Consequences
+    void Miss(int row, int col, vector<string> &grid);
+    void Hit(int row, int col, vector<string> &grid);
 };
 
 #endif
